14f05822fa3906772f3c5aa88bdf5642c46042fe
Per Thykjaer Jensen <per@multimusen.dk>
2017-02-02 13:19:04 +0100

    areas: menu, widget

--- functions.php
+++ functions.php
@@ -14,10 +14,10 @@
 /**
  * Set content width (media)
  */
-
+if ( ! isset( $content_width ) ) $content_width = 600;
 
 /**
- * Enqueue scripts
+ * Enqueue scripts (e.g. Bootstrap Jquery)
  */
 
 
@@ -25,50 +25,98 @@
  * Enqueue styles
  * tip: let style.css be the last one.
  */
-
+function themeslug_enqueue_style() {
+	wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css', 
+  false );
+}
+add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_style' );
 
 /**
  * Title tag
  */
-
+/**
+ * Title-tag
+ **/
+function theme_slug_setup() {
+	add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'theme_slug_setup' );
 
 /**
  * Automatic feed links
  */
-
+add_theme_support( 'automatic-feed-links' );
 
 /**
  * wp link pages
  */
-
+$defaults = array(
+	'before'           => '<p>' . __( 'Pages:' , "petj-mvp"),
+	'after'            => '</p>',
+	'link_before'      => '',
+	'link_after'       => '',
+	'next_or_number'   => 'number',
+	'separator'        => ' ',
+	'nextpagelink'     => __( 'Next page', "petj-mvp" ),
+	'previouspagelink' => __( 'Previous page' , "petj-mvp"),
+	'pagelink'         => '%',
+	'echo'             => 1
+);
+wp_link_pages( $defaults );
 
 /**
  * A menu
  */
-
+function register_my_menu() {
+  register_nav_menu('sidebar-menu',__( 'Sidebar Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
 
 /**
  * Widget area
  */
+function maat_widgets_init() {
 
+	register_sidebar( array(
+		'name'          => 'Home right sidebar',
+		'id'            => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>',
+	) );
+
+}
+add_action( 'widgets_init', 'maat_widgets_init' );
 
 /**
  * Costum header
  */
 
-
 /**
  * Costum background
  */
-
+$args = array(
+	'default-color' => '000000',
+	'default-image' => '%1$s/images/background.jpg',
+);
+add_theme_support( 'custom-background', $args );
 
 /**
  * Add editor styles
  */
-
+add_editor_style();
 
 /**
  * Theme thumbnails
  */
+add_theme_support( "post-thumbnails" );
 
+/**
+ * Add HTML5 theme support.
+ */
+function wpdocs_after_setup_theme() {
+    add_theme_support( 'html5', array( 'search-form' ) );
+}
+add_action( 'after_setup_theme', 'wpdocs_after_setup_theme' );
 ?>
--- index.php
+++ index.php
@@ -1,79 +1,158 @@
 <?php
 /**
+ * Basic HTML5 Markup
+ * Apart from that zero design!
  * @package: WordPress
  * @subpackage: petj-mvp
+ * 
+ * Normally this file would be separated into header, footer, etc.
  */
 ?>
-<!DOCTYPE html>
-    <html <?php language_attributes(); ?>>
-        <head>
-            <meta charset="<?php bloginfo( 'charset' ); ?>" />
-           
-            <title><?php wp_title(); ?></title>
-          
-            <link rel="profile" href="http://gmpg.org/xfn/11" />
-            <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-            <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-            <?php wp_head(); ?>
-            
-            <!-- add the stylesheet -->
-            <?php
-            /**
-             * Add the stylesheet 
-             */
-            wp_enqueue_style( 'style', get_stylesheet_uri() ); // loop for style.css and add it
-            ?>
 
-        </head>
+<!DOCTYPE html>
+  <html <?php language_attributes(); ?>>
+    <head>
+      <meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+        <!-- don't use a title tag, see functions.php "Title tag" -->          
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
+ 
+  </head>
 <body <?php body_class(); ?>>
 
-<!-- site header -->
-  <h1><?php bloginfo( 'name' ); ?></h1>
-  <h1><?php bloginfo( 'description' ); ?></h1>
-  <hr>
-<!-- site header -->
+<header>
+  <!-- header link to "home" -->
+  <a href="<?php bloginfo('url'); ?>">
 
-<!-- navigation -->
+    <!-- site name -->
+    <h1><?php bloginfo( 'name' ); ?></h1>
 
-<!-- /navigation -->
+    <!-- site description -->
+    <h2><?php bloginfo( 'description' ); ?></h2>
+  </a>
+</header> 
 
-<!-- some navigation (list pages, no menu) -->
-  <h3><?php _e('List pages (no menu)', 'petj-mvp'); ?></h3>
-  <hr>
+<main>
+  <article>
 
-  <ul>
-    <?php wp_list_pages( array( 'title_li' => '' ) ); ?>
-  </ul>
-<!-- /navigation -->
+  <?php 
+  /**
+   * The Loop
+   * @link: https://codex.wordpress.org/The_Loop
+   */
+  ?>
 
-<!-- loop -->
-<h3><?php _e('Loop Out All Pages (no paginaiton here)', 'petj-mvp'); ?></h3>
-<hr>
+   <!-- Start the Loop. -->
+   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php
-/**
- * A very basic loop ...
- */
-if ( have_posts() ) {
-	while ( have_posts() ) {
-	   the_post();
-  // (all content looped out)
-	//
-	the_content();
-	//
-	} // end while
-} // end if
-?>
-<!-- /loop -->
+   	<!-- Test if the current post is in category 3. -->
+   	<!-- If it is, the div box is given the CSS class "post-cat-three". -->
+   	<!-- Otherwise, the div box is given the CSS class "post". -->
 
-<?php wp_footer(); ?>
+   	<?php if ( in_category( '3' ) ) : ?>
+   		<div class="post-cat-three">
+   	<?php else : ?>
+
+      <!-- add the id and class for each post -->
+      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+   	
+   	<?php endif; ?>
+
+
+   	<!-- Display the Title as a link to the Post's permalink. -->
+
+   	<h2>
+      <a href="<?php the_permalink(); ?>" 
+        rel="bookmark" 
+        title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?>
+      </a>
+    </h2>
+
+
+   	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
+   	<small>
+      <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?>
+    </small>
+
+
+   	<!-- Display the Post's content in a div box. -->
+   	<div class="entry">
+   		<?php the_content(); ?>
+   	</div>
+
+
+   	<!-- Display a comma separated list of the Post's Categories. -->
+   	<p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+   	</div> <!-- closes the first div box -->
+
+
+
+
+   	<!-- Stop The Loop (but note the "else:" - see next line). -->
+   <?php endwhile; else : ?>
+
+   	<!-- The very first "if" tested to see if there were any Posts to -->
+   	<!-- display.  This "else" part tells what do if there weren't any. -->
+
+   	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+
+   	<!-- REALLY stop The Loop. -->
+   <?php endif; ?>
+
+
+  <?php 
+  /**
+   * ------- the loop end -------
+   */
+  ?>
+
+  </article>
+
+  <!-- the sidebar --> 
+  <aside id="sidebar">
+    <!-- search form -->
+    <?php get_search_form( ); ?>
+
+    <!-- Menu: "list style" -->  
+    <h3> <?php _e('Sidebar Menu','petj-mvp'); ?> </h3>
+    <?php wp_nav_menu( array( 'theme_location' => 'sidebar-menu' ) ); ?>
+
+    <!-- widget areas here -->
+    <h3> <?php _e('Widget area','petj-mvp'); ?> </h3>
+    <?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
+	    <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+		    <?php dynamic_sidebar( 'home_right_1' ); // here the widget is printed  ?>
+	    </div><!-- #primary-sidebar -->
+
+      <!-- List all pages -->
+      <h3><?php _e('Page List', 'petj-mvp'); ?></h3>
+        <ul>
+          <?php wp_list_pages( array( 'title_li' => '' ) ); ?>
+        </ul>
+
+    <?php endif; ?>
+  </aside>
+
+</main><!-- end main content -->
+
+<footer class="aligncenter">
+   &copy; GPLv3 Per T. Jensen <?php echo date('Y'); ?>
+</footer>
+
 
 <!-- JavaScript sample -->
 <script type="text/javascript">
 /* <![CDATA[ */
 // content of your Javascript goes here
+  console.log('JavaScript: ok!');
 /* ]]> */
 </script>
 
+<?php wp_footer(); // load this after your scripts ?>
 </body>
 </html>
