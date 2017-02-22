75f95731618b063cd2086427ceed3f11b4dbfb05
asathoor <asathoor@gmail.com>
2017-02-22 22:02:32 +0100

    nicer footer

--- footer.php
+++ footer.php
@@ -1,46 +1,46 @@
-<footer class="aligncenter">
-   &copy; GPLv3 Per T. Jensen <?php echo date('Y'); ?>
-</footer>
+	<footer class="aligncenter">
+		 &copy; GPLv3 Per T. Jensen <?php echo date('Y'); ?>
+	</footer>
 
-</div>
+</div><!-- /wrapper -->
 
 
 <!-- JavaScript sample -->
-<script type="text/javascript">
-/* <![CDATA[ */
-// content of your Javascript goes here
-  console.log('JavaScript: ok!');
-/* ]]> */
-</script>
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	// content of your Javascript goes here
+		console.log('JavaScript: ok!');
+	/* ]]> */
+	</script>
 
 
-<script>
-/**
- * Jquery Sample
- */
-( function( $ ) {
+<!-- Jquery: how to ... -->
+	<script>
 
-	// code goes here
-	console.log('Jquery: all systems up and running. Alert level: green.');
+	( function( $ ) {
 
-	// hide the children
-	$('.children').toggle();
+		// code goes here
+		console.log('Jquery: all systems up and running. Alert level: green.');
+
+		// hide the children
+		$('.children').toggle();
 	
-	// Based on @url: http://callmenick.com/post/slide-down-menu-with-jquery-and-css
-	$( '.page_item_has_children' ).hover(
-		      function(){
-		          $(this).children('.children').slideDown(200);
-		      },
-		      function(){
-		          $(this).children('.children').slideUp(200);
-		      }
-		  );
+		// Based on @url: http://callmenick.com/post/slide-down-menu-with-jquery-and-css
+		$( '.page_item_has_children' ).hover(
+				    function(){
+				        $(this).children('.children').slideDown(200);
+				    },
+				    function(){
+				        $(this).children('.children').slideUp(200);
+				    }
+				);
 
 
-} )( jQuery ); // jquery end
+	} )( jQuery ); // jquery end
 
-</script>
+	</script>
 
-<?php wp_footer(); // load this after your scripts ?>
+	<?php wp_footer(); // load this after your scripts ?>
+
 </body>
 </html>
