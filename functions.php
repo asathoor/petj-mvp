ac0ce02174e7b043983c21b35f327dd2a633bea6
asathoor <asathoor@gmail.com>
2017-03-11 21:14:24 +0100

    comstum images

--- functions.php
+++ functions.php
@@ -122,4 +122,32 @@ function wpdocs_after_setup_theme() {
     add_theme_support( 'html5', array( 'search-form' ) );
 }
 add_action( 'after_setup_theme', 'wpdocs_after_setup_theme' );
+
+/**
+ * Costum logo support
+ * @url: https://codex.wordpress.org/Theme_Logo
+ */
+function petj_mvp_setup() {
+	
+	add_theme_support( 'custom-logo', array(
+		'height'      => 100,
+		'width'       => 400,
+		'flex-width' => true,
+	) );
+
+}
+add_action( 'after_setup_theme', 'petj_mvp_setup' );
+
+/**
+ * Costum header image
+ * (load via style.css)
+ */
+$petjHeaderImage = array(
+	'flex-width'    => true,
+	'width'         => 820,
+	'flex-height'    => true,
+	'height'        => 200,
+	'default-image' => get_template_directory_uri() . '/images/header.jpg',
+);
+add_theme_support( 'custom-header', $petjHeaderImage );
 ?>
